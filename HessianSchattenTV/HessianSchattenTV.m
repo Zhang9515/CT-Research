c6a96318e9b60259d9dbcdc0702c65f6b67b66fc
Zhang9515 <zhang9515@sjtu.edu.cn>
2018-11-22 11:05:36 +0800

    1122

--- HessianSchattenTV/HessianSchattenTV.m
+++ HessianSchattenTV/HessianSchattenTV.m
@@ -47,61 +47,92 @@ LBeta = length ( BetaScanRange ) ;
 
 % CovarianceMatrix = eye( LBeta * LXigama * LP ) ;
 
-%% 
-alpha = 3e4 ; Tao = 1 ; LipschitzConstant = 144 * Tao^2 ; ps =3; Knest0 = 1; Kmm0 = 1; ProjectionOnBallofBsq0 = zeros( 3 , 3 , t_length * s_length * z_length ) ;
-Tmm = 10;   % outerloop
-Tnest = 10 ; % innerloop
+%% FDK as initial input
 
-picvector = reshape (pic, t_length * s_length * z_length, 1) ;
-V = ProjectionCone_3D (picvector, t_length, s_length, z_length, Size, BetaScanRange, Pdomain, Xigamadomain, Distance) ;
+picvector = reshape (pic, t_length * s_length * z_length, 1);
+clear pic;
+V = ProjectionCone_3D (picvector, t_length, s_length, z_length, Size, BetaScanRange, Pdomain, Xigamadomain, Distance);
+% R = reshape ( R , LP , LXigama, LBeta ) ;
+% figure,imshow3Dfull(R,[])
+% clear picvector;
+
+% z_length = 30;
+FDKresult = FDK ( R , Xigamadomain , Pdomain , BetaScanRange , Distance, Size, t_length, s_length, z_length) ;
+FDKresult = reshape ( FDKresult , t_length , s_length , z_length ) ;
+
+%% hybrid HS with TV iterative method
+
+alpha = 3e4 ; Tao = 1 ; LipschitzConstant = 144 * Tao^2 ; ps =3; 
+Knesterov0 = 1; Kmm0 = 1; ProjectionOnBallofBsq0 = zeros( 3 , 3 , t_length * s_length * z_length ) ;
+U0 = FDKresult ; 
+
+Tmm = 10;   % outerloop
+Tnesterov = 10 ; % innerloop
+
+AU = ProjectionCone_3D (FDK, t_length, s_length, z_length, Size, BetaScanRange, Pdomain, Xigamadomain, Distance) ;
 
 Kmmprevious = Kmm0 ;
+Cfai0 = ObjectionFunction( V , AU , ) ;
+Uprevious = U0 ; 
+Xprevious = U0 ;
+
 for tmm = 1 : Tmm
-    ProjectionData = ProjectionCone_3D (Ucurrent, t_length, s_length, z_length, Size, BetaScanRange, Pdomain, Xigamadomain, Distance) ;
+    ProjectionData = ProjectionCone_3D (Xprevious, t_length, s_length, z_length, Size, BetaScanRange, Pdomain, Xigamadomain, Distance) ;
 
     Residual = Backprojection( V - ProjectionData , Xigamadomain , Pdomain , BetaScanRange , Distance, Size, t_length, s_length, z_length) ;
 
-    Z = Ucurrent + alpha^(-1) * Residual ; 
+    Z = Xprevious + alpha^(-1) * Residual ; 
 
-    ProjectionOnRN = Z - Tao * HstarOmega( ProjectionOnBallofBsq , ps) ; 
-
-    GradientofG = Tao * HessianLOG3D( ProjectionOnRN ) ;
-
-      %% Nesterov method: orthogonal projections , to update omega, we use the result of ProjectionOnBallofBsq as 
+    %% Nesterov method: orthogonal projections , to update omega, we use the result of ProjectionOnBallofBsq as 
       % Omega
-    Knestprevious = Knest0 ; ProjectionOnBallofBsqprevious = ProjectionOnBallofBsq0 ;
-    for tnest = 1 : Tnest
+    Knesterovprevious = Knesterov0 ; ProjectionOnBallofBsqprevious = ProjectionOnBallofBsq0 ;
+    Omegaprevious = ProjectionOnBallofBsq0 ; 
+    
+    for tnesterov = 1 : Tnesterov
+          
+          ProjectionOnRN = Z - Tao * HstarOmega( Omegaprevious , ps) ; 
+          ProjectionOnRN = reshape( ProjectionOnRN , t_length , s_length , z_length ) ;
+    
+          GradientofG = Tao * HessianLOG3D( ProjectionOnRN ) ;
           ProjectionOnBallofBsq = Omegaprevious + GradientofG / LipschitzConstant ; 
           for n = 1 : t_length * s_length * z_length
 
-                % q = 2
+                % q = 2, p =2 , 1/p + 1/q = 1
                 Fnorm = norm ( ProjectionOnBallofBsq( : , : , n ), 'fro' ) ;
                 if ( Fnorm > 1 )
                     ProjectionOnBallofBsq( : , : , n ) = ProjectionOnBallofBsq( : , : , n ) / Fnorm ;
                 end
 
-                % q = inf
+                % q = inf , p =1, 1/p + 1/q = 1
             %     [ Usvd, ProjectionOnBallofBsq, Vsvd ] = svd ( ProjectionOnBallofBsq( : , : , n ) ) ; 
 
           end %n
-          Knestcurrent = ( 1 + sqrt( 1 + Knestprevious^2 ) ) / 2 ;
-          Omegacurrent = ProjectionOnBallofBsqprevious + ( Knestprevious - 1 ) / Knestcurrent .* ( ProjectionOnBallofBsq - ProjectionOnBallofBsqprevious ) ;
-          Knestprevious = Knestcurrent ;
+          Knesterovcurrent = ( 1 + sqrt( 1 + Knesterovprevious^2 ) ) / 2 ;
+          Omegacurrent = ProjectionOnBallofBsqprevious + ( Knesterovprevious - 1 ) / Knesterovcurrent .* ( ProjectionOnBallofBsq - ProjectionOnBallofBsqprevious ) ;
+          Knesterovprevious = Knesterovcurrent ;
           Omegaprevious = Omegacurrent;
           
     end  %nest
     S = Z - Tao * HstarOmega( OmegaCurrent , ps) ;
+    AS = ProjectionCone_3D (S, t_length, s_length, z_length, Size, BetaScanRange, Pdomain, Xigamadomain, Distance) ;
     Kmmcurrent = ( 1 + sqrt( 1 + Kmmprevious^2 ) ) / 2 ;
     
+    % determine whether the obectivefunction
+    Cfaicurrent = ObjectiveFunction( ProjectionData, AS , S, OrderofHS, Tao) ;
+    if ( Cfaicurrent > Cfaiprevious )
+        Ucurrent = Uprevious ;
+    else
+        Ucurrent = S ;
+    end
+    Xcurrent = Ucurrent + ( Kmmprevious / Kmmcurrent ) * ( S - Ucurrent ) + ( Kmmprevious - 1) / Kmmcurrent * ( Ucurrent - Uprevious ) ;
     
-    
+    Cfaiprevious = Cfaicurrent ;
+    Xprevious = Xcurrent ; 
     Kmmprevious = Kmmcurrent ; 
 end  %mm
 
-
-% R = reshape ( R , LP , LXigama, LBeta ) ;
-% figure,imshow3Dfull(R,[])
-
-
+Display = Ucurrent ; 
+Display = reshape( Display , t_length, s_length, z_length ) ;
+figure,imshow3Dfull ( Display , [] )
 
 toc
--- HessianSchattenTV/ObjectionFunction.m
+++ HessianSchattenTV/ObjectionFunction.m
@@ -1,7 +1,5 @@
-function Fai = ObjectionFunction( )
-
-
-
-
+function Fai = ObjectiveFunction( ProjectionData, AU , U, OrderofHS, Tao)
+    Hsnorm
+    Fai = 0.5 * (ProjectionData - AU)' * (ProjectionData - AU) + Tao * Hsnorm ;
 
 end
