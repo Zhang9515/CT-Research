2f85ee636405592d4348f8e212bb7356242d7b0f
Zhang9515 <zhang9515@sjtu.edu.cn>
2019-01-24 10:20:01 +0800

    0124

--- test_for_backprojection.m
+++ test_for_backprojection.m
@@ -35,7 +35,7 @@ clear ThoraxHD
 % Size = [ t_length * 0.7480 ; s_length * 0.7480 ; z_length * 1 ] ;     % actual range ( should refer to dicom info)
 Size = [ 60 , 60 , 60 * z_length / t_length ] ;
 
-z_lengthrec = 256 ;    % reconstruction matrix size
+z_lengthrec = z_length ;    % backprojection matrix size ( usually same as the size of original image) 
 Sizerec = [ 60 , 60 , 60 * z_lengthrec / t_length ] ;     % reconstruction real size
 
 Rplane = Size(1) * sqrt ( 2 ) / 2 ;                    %  circumscribed circle radius of project in the plane
@@ -44,13 +44,13 @@ Resolution = [ Size(1) / t_length , Size(2) / s_length , Size(3) / z_length ];
 
 Distance = 207.6 ;              % distance between source and center point ( 207.6 for 60 )
 
-PInt = 0.1 ;                                    %interval of P ( 0.1 exact )
+PInt = 0.2 ;                                    %interval of P ( 0.1 exact )
 MaxP = max ( ( Size(1) / 2 * Distance ) / ( Distance - Size(1) / 2 ) , Rplane * 1.1 );
 Pdomain = - MaxP : PInt : MaxP ;                       % detective range P
 Pdomain = single(Pdomain');
 LP = length ( Pdomain ) ;
 
-XigamaInt = 0.1 ;                                      % interval of Xigama ( 0.1 exact )
+XigamaInt = 0.2 ;                                      % interval of Xigama ( 0.1 exact )
 MaxXigama = ( Sizerec(3) / 2 * Distance ) / ( Distance - Size(1) * sqrt(2) / 2 ) ;       % computed by rule of similar triangle
 MaxXigama = 46.669 ; 
 Xigamadomain = - MaxXigama : XigamaInt : MaxXigama ;                       % detective range Xigama
@@ -60,7 +60,7 @@ LXigama = length ( Xigamadomain ) ;
 Center_t = Size(1) / 2 ;  Center_s = Size(2) / 2 ;   Center_z = Size(3) / 2 ;          % define the center 
 
 % FanAmax = atan ( MaxP / ( Distance - Rplane ) ) ; 
-BetaScanInt = deg2rad(1) ;             % scanning internal    ( 0.3 exact )           
+BetaScanInt = deg2rad(4) ;             % scanning internal    ( 0.3 exact )           
 % MaxBeta = 180 + 2 * FanAmax * 180 / pi ;         % short scan 
 MaxBeta = deg2rad(360) ;
 BetaScanRange = BetaScanInt : BetaScanInt : MaxBeta  ;     % scanning range , angle between SO and aixs Y
@@ -100,7 +100,7 @@ for  split = 1 : times
     %% GPU accelerate the preweight & filteration & backprojection
 
      R= single(R) ;
-    Display = Backprojection ( R , Xigamadomain , Pdomain , BetaScanRangePrime , Distance, Sizerec, t_length, s_length, z_lengthrec) ;
+    Display = Display + Backprojection ( R , Xigamadomain , Pdomain , BetaScanRangePrime , Distance, Sizerec, t_length, s_length, z_lengthrec) ;
     clear R ;
 end
 Display = reshape ( Display , t_length , s_length , z_lengthrec ) ;
