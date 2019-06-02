1c42bc3859b11c6a5f2458106d57824da576c47f
Zhang9515 <zhang9515@sjtu.edu.cn>
2019-06-02 12:06:43 +0800

    0602

--- AdpativePatchSizeSelection.m
+++ AdpativePatchSizeSelection.m
@@ -13,34 +13,35 @@ GX = GradientxMatrix * double(picvector) ;
 GY = GradientyMatrix * double(picvector) ;
 GradAng = rad2deg(atan2( GY , GX )) ;
 PatchSize = zeros( numel(pic), 1 ) ;       % store the patch size corresponding to each pixel
-edges = -180 : 10 : 180 ;
-threshold = 0 ;      % KL threshold
+KLprevious = zeros( numel(pic), 1 ) ;    % store the intermediate variable for each pixel
+
+edges = -180 : 30 : 180 ;      % bin interval of the gradient angle distribution
+threshold = 1e-3 ;      % KL threshold
 hist = zeros( numel(pic), numel(edges)-1 ) ;     % store the histogram of gradient angle
+Pprevious = zeros( numel(pic), numel(edges)-1  ) ;    % store the intermediate variable for each pixel
+
 for pz = 1 : 5
     %
-   
     %
     for x = 1+pz : width-pz
         for y = 1+pz : height-pz          % ordinary area
             %
-            x = 42 ; y = 126 ;
             %
             center_index =  x + (y-1) * width ;
             if (pz == 1)
                 index = kron(-pz:pz,ones(2*pz+1,1)) + kron(ones(1,2*pz+1),width * (-pz:pz)') + center_index;
                 hist(center_index,:) = histcounts( GradAng ( index ) , edges ) ;
-                Pprevious = hist(center_index,:)   / sum(hist(center_index,:) ) ;
+                Pprevious( center_index , : ) = hist(center_index,:)   / sum(hist(center_index,:) ) ;
                 PatchSize( center_index ) = 2*pz + 1 ;
-                KLprevious = 0 ;
             else
                 index = [ (-pz : pz) + pz * width, (-pz : pz) - pz * width, -pz + (-pz:pz)*width, pz + (-pz:pz)*width] + center_index ;
-                hist(center_index,:)  = hist(center_index,:)  + histcounts( GradAng ( index ) , edges ) ;
-                Pcurrent = hist(center_index,:) / sum(hist(center_index,:) )  ;
-                KLcurrent = kldiv( 1:(numel(edges)-1) , Pprevious + eps, Pcurrent + eps, 'sym') ;      % compute KL divergence between current and previous GradAng distribution
-                Pprevious = Pcurrent ;
-                if ( (KLcurrent - KLprevious) > threshold)     % if current KL divergence is larger enough than the previous, we select the current patch size
-                    KLprevious = KLcurrent ;
-                    PatchSize( center_index ) = 2*pz + 1 ;
+                Pcurrent = histcounts( GradAng ( index ) , edges ) / sum( histcounts( GradAng ( index ) , edges ) ) ;
+                KLcurrent = kldiv( 1:(numel(edges)-1) , Pprevious( center_index , : ) + eps, Pcurrent + eps, 'sym') ;      % compute KL divergence between current and previous GradAng distribution
+                hist(center_index,:)  = hist(center_index,:)  + histcounts( GradAng ( index ) , edges ) ;                
+                Pprevious( center_index , : ) = hist(center_index,:) / sum( hist(center_index,:) ) ;
+                if ( (KLcurrent - KLprevious( center_index ) ) > threshold)     % if current KL divergence is larger enough than the previous, we select the current patch size
+                    KLprevious( center_index ) = KLcurrent ;
+                    PatchSize( center_index ) = 2*(pz-1) + 1 ;
                 end
             end    
         end      
