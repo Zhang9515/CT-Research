a1375a717b99764cbb982d55905bc70ff5f94090
Zhang9515 <zhang9515@sjtu.edu.cn>
2019-09-15 20:04:49 +0800

    0915

--- MatlabCpp/Img2vec_Mat2Cpp2D.m
+++
@@ -1,3 +0,0 @@
-function picvector = Img2vec_Mat2Cpp2D( Img )
-    picvector = single( reshape ( fliplr(Img'), numel(Img) , 1) ) ;
-end
--- MatlabCpp/Vec2img_Cpp2Mat2D.m
+++
@@ -1,4 +0,0 @@
-function img = Vec2img_Cpp2Mat2D( imgvector , height , width )
-    img = reshape( imgvector , height , width ) ;
-    img = fliplr(img)' ;
-end
