afeb3048c11572ae5660eae5c80cf0e90a1540eb
Zhang9515 <zhang9515@sjtu.edu.cn>
2019-09-16 01:06:20 +0800

    0916

--- My_novel_algorithm/AdaptivePatch/TVpatchpriorPICCS.m
+++ My_novel_algorithm/AdaptivePatch/TVpatchpriorPICCS.m
@@ -10,7 +10,7 @@ clear;
 % server2 path
 load ..\Data\trial2D
 load ..\Data\trial2D_prior_360
-save_path = '..\Data\0712\' ;
+save_path = '..\Data\PICCS_miu200_lamda0.001_p4s3sp5_CGzero_init\' ;
 % load 'E:\ZXZ\Data\trial2D_angle5'
 % display parameter
 displaywindow = [0 0.5] ;
@@ -40,12 +40,10 @@ R = zeros ( Lt ,  Ltheta ) ;   % create space to store fan projection
 %% compute system matrix
 
 % SysMatrix = GenSysMatParal ( height , width , Size , Center_x , Center_y , thetaRange , t_range ) ;
-
 load SysMatrix
 picvector = Img2vec_Mat2Cpp2D( pic ) ;  % original image
 clear pic
 R = SysMatrix * double(picvector) ;        % generate projection with system matrix
-
 % R = reshape( R , Lt , Ltheta ) ;
 % figure,imshow(R,[])
 % Norm = norm ( R ) ;
@@ -64,8 +62,8 @@ Threshold = 1e-6 ;
 innerTimes = 15 ;
 MaxLim = 1 ; MinLim = 0 ;    % value limit
 miu = 200 ;        % regularization parameter for TV
-lamda1 = 0.001 * miu ;           lamda2 = 0.001 * miu ;            % relaxtion factor for split bregman( 2*miu is recommended by the classical paper)
 alpha = 0.5 ;    % panelty balance between the prior and the current 
+lamda1 = 0.001 * miu ;           lamda2 = 0.001 * miu ;            % relaxtion factor for split bregman( 2*miu is recommended by the classical paper)
 Display = zeros ( height * width , 1 ) ;          % store the reconstruction
 LDisplay = numel ( Display ) ;
 
@@ -86,7 +84,7 @@ gradientMatrix_y = gradient2Dmatrix_y(height,width);
 % preparation for CG
 divergence_matrix = divergenceMatrix2D(height,width);
 b1_CG = miu * (SysMatrix') * R ; 
-iter_CG = 100 ;
+iter_CG = 500 ;
 % img_init = zeros(LDisplay,1) ; 
 
 % patch operation parameter
@@ -118,34 +116,57 @@ for outerloop = 1 : outeriter
     drawnow ;
     Display_prior = double ( Img2vec_Mat2Cpp2D( Image2D ) );
     
-    % initial of inner SB iterative
+    % initial of inner SB iterative, all parameter should be initialized
+    % based on zero input
     dx1 = zeros(LDisplay,1); dy1 = zeros(LDisplay,1); bx1 = zeros(LDisplay,1); by1 =zeros(LDisplay,1);   
     dx2 = gradientMatrix_x * (-Display_prior) ; dy2 = gradientMatrix_y * (-Display_prior) ; bx2 = zeros(LDisplay,1); by2 =zeros(LDisplay,1);   
+    % initial of range limitation parameter
+    % parameter initiation in the range limitation ( min < x < max)
+    miu1 = zeros(LDisplay,1) ;
+    miu2 = zeros(LDisplay,1) ;
+    xig1 = 0.1 * 2^0;
+    xig2 = 0.1 * 2^0;
+    rate1 = 2 ; 
+    rate2 = 2 ;
     
     local_e = 100 ;     % initial
     IterativeTime = 1  ;      % times to iterative
+    
+    Display = zeros(LDisplay,1) ;
     %% split bregman to solve tv-based problem
     while ( IterativeTime <= innerTimes && local_e > Threshold )            % end condition of loop
                  Display_previous = Display ;
+                 % introduce the range limitation into the iterative
+                 % framework
+%                  Display_det1 = max( 0 , miu1 - xig1 * ( Display_previous - MinLim ) ) ;  Display_det1 = Display_det1 ./ ( Display_det1 + eps ) ;
+%                  Display_det2 = max( 0 , miu2 - xig2 * ( -Display_previous + MaxLim ) ) ;  Display_det2 = Display_det2 ./ ( Display_det2 + eps ) ;
+%                  Display_det1 = xig1 * sparse( 1:LDisplay, 1:LDisplay, Display_det1, LDisplay , LDisplay ) ;
+%                  Display_det2 = xig2 * sparse( 1:LDisplay, 1:LDisplay, Display_det2, LDisplay , LDisplay ) ;
+                 Display_det1 = 0 ; Display_det2 = 0 ;
+                 
                  b_CG = b1_CG + lamda1 * ( gradientMatrix_x * ( dx1 - bx1 ) + gradientMatrix_y * ( dy1 - by1 ) ) ... 
-                 + lamda2 * ( gradientMatrix_x * ( dx2 + gradientMatrix_x * Display_prior - bx2 ) + gradientMatrix_y * ( dy2 + gradientMatrix_y * Display_prior - by2 ) ) ;
+                 + lamda2 * ( gradientMatrix_x * ( dx2 + gradientMatrix_x * Display_prior - bx2 ) + gradientMatrix_y * ( dy2 + gradientMatrix_y * Display_prior - by2 ) ) ...
+                 + Display_det1 * ( xig1 * MinLim + miu1 ) + Display_det2 * ( xig2 * MaxLim - miu2 ) ;
                   
-                 iter_CG = 500 ;
                  % here are two choices: 1. using the previous result; 2. using zero initialization
                  % To solve Ax = b , the paramter matrix A is already a
                  % semidefinite full-rank matrix, so there is no need to
                  % use norm equation
-%                  systemfunc = @(vec,tt) miu * (SysMatrix') * ( SysMatrix * vec ) + (lamda1 + lamda2) * divergence_matrix * vec ;
-%                  [Display,flag,resNE,iter] = cgls(systemfunc, b_CG, 0, 1e-5, iter_CG, true, Display_previous) ;
-                 Display = cgls4TV ( SysMatrix, divergence_matrix , b_CG , iter_CG , miu , lamda1 + lamda2 , Display_previous) ;        
+                 systemfunc = @(vec,tt) miu * (SysMatrix') * ( SysMatrix * vec ) + (lamda1 + lamda2) * divergence_matrix * vec ;
+%                  [Display,flag,resNE,iter] = cgls(systemfunc, b_CG, 0, 1e-6, iter_CG, true, Display_previous) ;
+                 [Display,flag,resNE,iter] = cg(systemfunc, b_CG, 1e-6, iter_CG, true, Display_previous,double(picvector)) ;
                  
+%                  Display = cg4TV ( SysMatrix, divergence_matrix , Display_det1, Display_det2, b_CG , iter_CG , miu , lamda1 + lamda2 , Display_previous ) ;        
+%                  Display = cgls4TV ( SysMatrix, divergence_matrix , b_CG , iter_CG , miu , lamda1 + lamda2 , Display_previous) ;        
+                 miu1 = max(0 , miu1 - xig1 * (Display - MinLim)) ; miu2 = max( 0 , miu2 - xig2 * (-Display + MaxLim ) ) ;
+                 xig1 = xig1 * rate1 ; xig2 = xig2 * rate2 ;
                  Display ( Display < MinLim ) = MinLim ;       Display ( Display > MaxLim ) = MaxLim ;   % non-negation constraint
                  
                  Substract_Display = Display - Display_prior ;
-                 dx1 = soft_threshold( gradientMatrix_x * Display + bx1 , alpha/lamda1);         % split bregman update
-                 dy1 = soft_threshold( gradientMatrix_y * Display + by1 , alpha/lamda1);
-                 dx2 = soft_threshold( gradientMatrix_x * Substract_Display + bx2 , (1-alpha)/lamda2);         % split bregman update
-                 dy2 = soft_threshold( gradientMatrix_y * Substract_Display + by2 , (1-alpha)/lamda2);
+                 dx1 = soft_threshold( gradientMatrix_x * Display + bx1 , alpha/(lamda1+eps));         % split bregman update
+                 dy1 = soft_threshold( gradientMatrix_y * Display + by1 , alpha/(lamda1+eps));
+                 dx2 = soft_threshold( gradientMatrix_x * Substract_Display + bx2 , (1-alpha)/(lamda2+eps));         % split bregman update
+                 dy2 = soft_threshold( gradientMatrix_y * Substract_Display + by2 , (1-alpha)/(lamda2+eps));
                  bx1 = bx1 - dx1 + gradientMatrix_x * Display ; 
                  by1 = by1 - dy1 + gradientMatrix_y * Display ; 
                  bx2 = bx2 - dx2 + gradientMatrix_x * Substract_Display ; 
@@ -160,7 +181,7 @@ for outerloop = 1 : outeriter
                  disp ( ['IterativeTime: ', num2str(IterativeTime), ';   |    RMSE: ', num2str(rmse ( IterativeTime ,outerloop)), ';   |    psnr: ', num2str(PSNR ( IterativeTime , outerloop)), ';   |    local_e: ', num2str(local_e), ';   |    Loss: ', num2str(loss)]) ;
                  disp( ['SplitBregman_Constraint_X1: ',  num2str(norm(dx1-gradientMatrix_x * Display,2)), '  Constraint_Y1: ', num2str(norm(dy1-gradientMatrix_y * Display,2)), ...
                      '  Constraint_X2: ', num2str(norm(dx2-gradientMatrix_x * Substract_Display,2)), '  Constraint_Y2: ' , num2str(norm(dy2-gradientMatrix_y * Substract_Display,2))] )
-                 %     plot ( 2 : IterativeTime , rmse ( 2  : IterativeTime ) ) ;
+    %     plot ( 2 : IterativeTime , rmse ( 2  : IterativeTime ) ) ;
     %     ylim ( [ 0 , ( 10 * rmse ( IterativeTime ) ) ] ) ;
     %     drawnow ;           
 
